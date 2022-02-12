7dbd4b3f63aa7c0418be547f8716bb8d536fc6d4
CaptainQuickWit <cppatel720@gmail.com>
2022-02-11 23:54:42 -0500

    added navbar & updated package loc json filek

--- src/app/event-app.component.ts
+++ src/app/event-app.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'events-app',
   //templateUrl: './app.component.html',
-  template:`<events-list></events-list>
+  template:`
+      <nav-bar></nav-bar>
+      <events-list></events-list>
   `,
   /**
    * Style url will be referenced at a later time
